c52b0908317d42ba9b8177600a371101773d8303
Francisco Montenegro <franciscomontenegro22@gmail.com>
2022-05-24 10:51:02 -0300

    fix index

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -39,7 +39,7 @@
                                                     @csrf
                                                     @method('DELETE')
                                                     <button type="submit" class="btn btn-danger">Delete</button>
-                                                </form> --}}
+                                                </form> --}}    
                                         </tr>
                                     @endforeach
                                 </tbody>
